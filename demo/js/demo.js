640c1ab580d4d8cdc631224a77f68a50b686d94a
Timothy Nguyen <timn@celticchicago.com>
2016-04-19 14:43:32 -0500

    content update and smoother transition between content and intro

--- demo/js/demo.js
+++ demo/js/demo.js
@@ -1,15 +1,27 @@
 $(document).ready(function() {
+	//continue button
 	$('aside button').on('click', function() {
-		toggleStartingPos();
+		slideToContent();
 	});
 
+	//back button
 	$('.button-group a:last-child').on('click', function() {
-		toggleStartingPos();
+		$('.content').addClass('pushed-down');
+		//promise is used here to queue the animation and callback because slideToIntro was being called twice
+		$('html, body').animate({scrollTop: 0}, 900).promise().done(function () {
+			slideToIntro();
+		});
 	});
 
-	function toggleStartingPos() {
-		$('aside').toggleClass('invisible');
-		$('.page-wrapper').toggleClass('no-flow');
-		$('.content').toggleClass('pushed-down');	
+	function slideToContent() {
+		$('aside').addClass('invisible');
+		$('.page-wrapper').removeClass('no-flow');
+		$('.content').removeClass('pushed-down');	
+	}
+
+	function slideToIntro() {
+		$('.page-wrapper').addClass('no-flow');
+		$('aside').removeClass('invisible');
+		
 	}
 });
